78385e32b88bc213bbfb756d27cf70d29adf2565
Prata <94478762+Pietrocastro@users.noreply.github.com>
2024-01-18 19:40:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
-//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
-//- "print": imprime um texto de saída (output), pulando linha.
-const prompt = require("prompt-sync")();
-// Para converter os valores inseridos na entrada, utilizamos a função JavaScript parseInt() que converte a parte inicial da string em um número inteiro.
-let heroName = prompt("Tell me, what's your name? ");
-const inicialPosition = parseInt(prompt("Tell the position that the hero started: "));
-const totalSteps = parseInt(prompt("Now, tell how many steps will be taken: "));
+const prompt = require("prompt-sync")()
 
-//TODO: Calcule a posição final do herói:
-const finalPosition = inicialPosition + totalSteps;
+console.log("WELCOME TO HERO POSITION [by Pietro Prata]")
+
+let heroName = prompt("Tell me, what's your name? ")
+const inicialPosition = parseInt(prompt("Tell the position that the hero started: "))
+const totalSteps = parseInt(prompt("Now, tell how many steps will be taken: "))
+
+//Calcula a posição final do herói:
+const finalPosition = inicialPosition + totalSteps
 
 // Imprime a posição final
-console.log("Hero's final position: ", finalPosition);
+console.log("Hero's final position: ", finalPosition)
